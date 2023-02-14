bb3e0c975a798cc9f4afc1f04ec18151cd6b1ad9
Bohdan Horbyk <113856009+Bohdanhorbyk@users.noreply.github.com>
2023-02-14 22:10:51 +0200

    Slider - done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel(
-        {center:true,
-        loop:true}
-    );
-  });
+
+
+  const owl = $('.owl-carousel');
+owl.owlCarousel({center:true,
+    loop:true,
+    margin:30,
+    startPosition: 0,
+    items: 3,
+});
+
+
+// Go to the previous item
+$('.slider__btn-prev').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel');
+})
+
+
+// Go to the next item
+$('.slider__btn-next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
